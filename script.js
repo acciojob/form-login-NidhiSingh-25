d16f7472b7ead7ba53ccde405015434c7e2a98e9
NidhiSingh-25 <nidhi630694@gmail.com>
2025-09-29 22:56:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
 
+	let form=document.getElementById("form1");
+
+	const firstName=document.querySelector('input[name="fname"]').value.trim();
+    const lastName =document.querySelector('input[name="lname"]').value.trim();
+
+	const fullName=firstName+" "+lastName;
+	alert(fullName);
 }
