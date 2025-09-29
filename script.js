592b131f2e399ed2964248e6e57ef9841e1a4ead
NidhiSingh-25 <nidhi630694@gmail.com>
2025-09-29 22:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function getFormvalue() {
     //Write your code here
 
-	let form=document.getElementById("form1");
-
 	const firstName=document.querySelector('input[name="fname"]').value.trim();
     const lastName =document.querySelector('input[name="lname"]').value.trim();
 
